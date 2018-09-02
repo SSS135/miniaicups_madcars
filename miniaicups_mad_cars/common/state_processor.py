e890006d7ff8efa33d5524e04295dc50b3018d2e
Alexander Penkin <sss13594@gmail.com>
2018-09-02 16:31:46 +0500

    fix

--- miniaicups_mad_cars/common/state_processor.py
+++ miniaicups_mad_cars/common/state_processor.py
@@ -1,44 +1,53 @@
 import math
 
 import numpy as np
-from ..common.types import TickStep, Car
+from ..common.types import TickStep, Car, NewMatchStep
 from ..common.vec2 import Vec2
 
 
 class StateProcessor:
-    state_size = 20 + 3
+    state_size = 8 * 3 + 4 + 3 + 6
     stacked_states = 4
     frameskip = 4
     pos_mean = Vec2(600, 150)
     pos_std = Vec2(1000, 400)
-    deadline_std = 300
+    deadline_std = 200
 
     def __init__(self, game_info):
-        self.game_info = game_info
+        self.game_info: NewMatchStep = game_info
         self.states = [np.zeros(self.state_size, dtype=np.float32) for _ in range(self.stacked_states)]
         self.frame_index = 0
 
     def update_state(self, tick: TickStep) -> np.ndarray or None:
         if self.frame_index % self.frameskip == 0:
-            state = [*self._get_car_state(tick.my_car),
-                     *self._get_car_state(tick.enemy_car),
-                     tick.deadline_pos / self.deadline_std,
-                     self.game_info.proto_car.external_id - 2,
-                     (self.game_info.proto_map.external_id - 3.5) / 2.5]
+            mc = np.array(self._get_car_state(tick.my_car))
+            ec = self._get_car_state(tick.enemy_car)
+            state = [
+                *mc,
+                *ec,
+                *np.sin((ec - mc) * math.pi * 10),
+                tick.deadline_pos / self.deadline_std,
+                -tick.deadline_pos / self.deadline_std,
+                (tick.my_car.pos.y - tick.deadline_pos) / 100,
+                (tick.enemy_car.pos.y - tick.deadline_pos) / 100,
+                *self._one_hot(self.game_info.proto_car.external_id - 1, 3),
+                *self._one_hot(self.game_info.proto_map.external_id - 1, 6),
+            ]
             state = np.array(state, dtype=np.float32)
-            self.states.pop(-1)
+            self.states.pop(0)
             self.states.append(state)
-            cur_state = np.array(self.states).flatten()
+            cur_state = np.concatenate(self.states).flatten()
         else:
             cur_state = None
         self.frame_index += 1
         return cur_state
 
+    def _one_hot(self, n, nmax):
+        return [int(n == i) for i in range(nmax)]
+
     def _get_car_state(self, c: Car):
         pos = self._norm_pos(c.pos)
         return (*pos,
-                math.sin(pos.x * 30),
-                math.cos(pos.y * 30),
                 *self._norm_pos(c.fw_pos),
                 *self._norm_pos(c.bw_pos),
                 math.sin(c.angle),
